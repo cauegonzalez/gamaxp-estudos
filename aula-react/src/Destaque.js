5cbbd0270a36c6ad68fa0de3b2267488625833ab
Cauê Gonzalez <cauegonzalez@gmail.com>
2019-03-04 17:08:41 -0300

    Criando projeto React e novo componente

---
+++ aula-react/src/Destaque.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Destaque = () => {
+  return (
+    <div>
+      <p>
+        <b>autor: </b>Cauê
+      </p>
+      <p>
+        <b>artigo: </b>Como ser Legal
+      </p>
+      <p>
+        <b>data: </b>04/03/2019
+      </p>
+    </div>
+  )
+}
+
+export default Destaque;
